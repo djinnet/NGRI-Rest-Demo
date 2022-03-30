40b0c3992db73062d4e87748a68c8157b80981e9
Djinnet renshira <lukas.alexander@hotmail.com>
2022-03-30 23:49:28 +0200

    Improved error handling & httpclient handler in blazor app.

---
+++ NGRI.Frontend/HttpClient/CustomHttpClient.cs
@@ -0,0 +1,177 @@
+﻿namespace NGRI.Frontend.HttpClient
+{
+    using System.Net.Http;
+    using System.Net.Http.Json;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.Logging;
+    using NGRI.Library.Model;
+
+
+    // this class is used to make the http calls to the backend
+    public class CustomHttpClient : ICustomHttpClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ILogger<CustomHttpClient> _logger;
+        private readonly string _baseUrl = "https://localhost:7046";
+
+        public CustomHttpClient(HttpClient httpClient, ILogger<CustomHttpClient> logger)
+        {
+            _httpClient = httpClient;
+            _logger = logger;
+        }
+
+        #region Estate
+        /// <summary>
+        /// Get all the estates from the backend
+        /// </summary>
+        /// <returns>Estates</returns>
+        public async Task<Estate[]?> GetEstatesAsync()
+        {
+            try
+            {
+                return await _httpClient.GetFromJsonAsync<Estate[]>($"{_baseUrl}/api/Estates");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+                return null;
+            }
+        }
+
+        //delete an estate
+        public async Task<bool> DeleteEstateAsync(int id)
+        {
+            try
+            {
+                var response = await _httpClient.DeleteAsync($"{_baseUrl}/api/Estates/{id}");
+                return response.IsSuccessStatusCode;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+                return false;
+            }
+        }
+
+        //get an estate
+        public async Task<Estate?> GetEstateAsync(int id)
+        {
+            try
+            {
+                return await _httpClient.GetFromJsonAsync<Estate>($"{_baseUrl}/api/Estates/{id}");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+                return null;
+            }
+        }
+
+        //update an estate
+        public async Task<bool> UpdateEstateAsync(Estate estate)
+        {
+            try
+            {
+                var response = await _httpClient.PutAsJsonAsync($"{_baseUrl}/api/Estates", estate);
+                return response.IsSuccessStatusCode;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+                return false;
+            }
+        }
+
+        //create an estate
+        public async Task<bool> CreateEstateAsync(Estate estate)
+        {
+            try
+            {
+                var response = await _httpClient.PostAsJsonAsync($"{_baseUrl}/api/Estates", estate);
+                return response.IsSuccessStatusCode;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+                return false;
+            }
+        }
+        #endregion
+
+        #region Condition Reports
+        // get all the reports from the backend with the given estate id
+        public async Task<ConditionReport[]?> GetReportsFromEstateID(int id)
+        {
+            try
+            {
+                return await _httpClient.GetFromJsonAsync<ConditionReport[]>($"{_baseUrl}/api/ConditionReports/GetReportsFromEstateID/{id}");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+                return null;
+            }
+        }
+
+        // delete a report
+        public async Task<bool> DeleteReportAsync(int id)
+        {
+            try
+            {
+                var response = await _httpClient.DeleteAsync($"{_baseUrl}/api/ConditionReports/{id}");
+                return response.IsSuccessStatusCode;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+                return false;
+            }
+        }
+
+        // get a report
+        public async Task<ConditionReport?> GetReportAsync(int id)
+        {
+            try
+            {
+                return await _httpClient.GetFromJsonAsync<ConditionReport>($"{_baseUrl}/api/ConditionReports/{id}");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+                return null;
+            }
+        }
+
+        // update a report
+        public async Task<bool> UpdateReportAsync(ConditionReport report)
+        {
+            try
+            {
+                var response = await _httpClient.PutAsJsonAsync($"{_baseUrl}/api/ConditionReports", report);
+                return response.IsSuccessStatusCode;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+                return false;
+            }
+        }
+
+        // create a report
+        public async Task<bool> CreateReportAsync(ConditionReport report)
+        {
+            try
+            {
+                var response = await _httpClient.PostAsJsonAsync($"{_baseUrl}/api/ConditionReports", report);
+                return response.IsSuccessStatusCode;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+                return false;
+            }
+        }
+        #endregion
+
+    }
+}
--- NGRI.Frontend/Program.cs
+++ NGRI.Frontend/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using NGRI.Frontend;
+using NGRI.Frontend.HttpClient;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -8,5 +9,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<ICustomHttpClient, CustomHttpClient>();
 
 await builder.Build().RunAsync();
